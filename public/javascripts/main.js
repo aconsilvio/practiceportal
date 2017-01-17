07baecf957b112a40ed4fd0727942f4dc282255c
Annabel <annabel.consilvio@gmail.com>
2017-01-17 16:02:03 -0500

    adding time stamps for each video, moving to pictures rather than iframes, fixing load more button

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -33,8 +33,8 @@ var DemoCtrl = function ($scope, $facebook, $http, $window) {
   $scope.navInstrument = "instruments";
   // var groupIDs = ["158881161246610"]
   // $scope.groupInstruments = ["oboe"]
-  var groupIDs = ["687498148076618", "222774521466864", "1517819058276334", "158881161246610", "1144863825582537", "1137083516407421", "363584057367097", "353600324976899", "1230747207018937", "224802994614669", "1435519730079849", "760242687459625", "1032810963489791", "1627655720873882", "1631363607173492", "205198266611379", "661301877370767", "1115432718579904"]
-  $scope.groupInstruments = ["voice", "tuba", "sax", "oboe", "trombome", "bassoon", "clarinet", "guitar", "piano", "flute", "viola", "cello", "bass", "precussion", "trumpet", "french horn", "violin", "sister"]
+  var groupIDs = ["687498148076618", "222774521466864", "1517819058276334", "158881161246610", "1144863825582537", "1137083516407421", "363584057367097", "353600324976899", "1230747207018937", "224802994614669", "1435519730079849", "760242687459625", "1032810963489791", "1627655720873882", "1631363607173492", "205198266611379", "661301877370767"]
+  $scope.groupInstruments = ["voice", "tuba", "sax", "oboe", "trombome", "bassoon", "clarinet", "guitar", "piano", "flute", "viola", "cello", "bass", "precussion", "trumpet", "french horn", "violin"]
   function toObject(names, values) {
     var result = {};
     for (var i = 0; i < names.length; i++)
@@ -56,7 +56,7 @@ var DemoCtrl = function ($scope, $facebook, $http, $window) {
               res.instrument = idDict[groupID]
               res.realtime = res.updated_time
               res.updated_time = new Date(res.updated_time).getTime();
-              
+              res.length = (res.length/60).toFixed(2).toString().replace(".", ":");
               // time difference in ms
               var timeDiff = currentTime - res.updated_time;
               // strip the ms
