5632dc61dcd61108893aa6c17c22c1b07417f721
Annabel <annabel.consilvio@gmail.com>
2017-01-18 16:25:48 -0500

    fixing app.js

--- app.js
+++ app.js
@@ -52,7 +52,6 @@ mongoose.connect(mongoURI);
 // app.post('/getNewVideos', videos.getNewVideos)
 // app.get("/getAllVideos", videos.getAllVideos)
 
-var PORT = process.env.PORT || 3000;
-    app.listen(PORT, function() {
-      console.log("Application running on port: ", PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
